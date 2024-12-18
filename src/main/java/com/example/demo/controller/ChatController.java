c7b5d258b06b86104f8a67c963d84b23ef376f5a
Claudia <148151287+CF-2003@users.noreply.github.com>
2024-12-18 03:19:43 +0100

    Update ChatController.java

--- src/main/java/com/example/demo/controller/ChatController.java
+++ src/main/java/com/example/demo/controller/ChatController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/chat")
 public class ChatController {
-    
+    //Chat
     private final List<ChatMessage> messages = new ArrayList<>();
     private final AtomicInteger lastId = new AtomicInteger(0);
 
